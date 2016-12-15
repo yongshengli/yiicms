7cddb62e51e975c0e6d1dd8f3a1c04fce821e05d
liyongsheng <liyongsheng@meicai.cn>
2016-12-15 10:50:08 +0800

    fix bug

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -91,9 +91,9 @@ class SiteController extends Controller
     public function actionSearch()
     {
         $keyword = Yii::$app->request->get('keyword');
-        if(empty($keyword)){
-//            $this->;
-        }
+//        if(empty($keyword)){
+////            $this->;
+//        }
         $query = Content::find()
             ->andFilterWhere(['in', 'type', [Content::TYPE_NEWS,Content::TYPE_PRODUCTS]])
             ->andFilterWhere(['like', 'title', $keyword])
