02acb7f0fd9ca7e25c2b9084eea208fa23e5949f
liyongsheng <liyongsheng@meicai.cn>
2016-12-15 17:07:18 +0800

    fix bug

--- config/params.php
+++ config/params.php
@@ -4,4 +4,17 @@ return [
     'adminEmail' => '739800600@qq.com',
     'pageSize' =>20,
     'logo'=>'@web/images/logo.png',
+    'nav'=>[
+        'options' => ['class' => 'navbar-nav'],
+        'items' => [
+            ['label' => '产品', 'url' => ['/products/list'],
+                'active'=>function(){return Yii::$app->controller->id=='products';}
+            ],
+            ['label' => '新闻', 'url' => ['/news/list'],
+                'active'=>function(){return Yii::$app->controller->id=='news';}
+            ],
+            ['label' => '关于我们', 'url' => ['/site/about'],],
+            ['label' => '联系我们', 'url' => ['/site/contact']],
+        ],
+    ],
 ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -56,22 +56,9 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-blue',
         ],
     ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => '产品', 'url' => ['/products/list'],
-                'active'=>function(){return Yii::$app->controller->id=='products';}
-            ],
-            ['label' => '新闻', 'url' => ['/news/list'],
-                'active'=>function(){return Yii::$app->controller->id=='news';}
-            ],
-            ['label' => '关于我们', 'url' => ['/site/about'],],
-            ['label' => '联系我们', 'url' => ['/site/contact']],
-        ],
-    ]);
+    echo Nav::widget(Yii::$app->params['nav']);
     NavBar::end();
     ?>
-
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
