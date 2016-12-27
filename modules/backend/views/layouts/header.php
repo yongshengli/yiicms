4b9c21ce4b0f28973fe0725aaa8a72c5ece4f5a7
liyongsheng <liyongsheng@meicai.cn>
2016-12-27 15:13:45 +0800

    fix bug

--- modules/backend/views/layouts/header.php
+++ modules/backend/views/layouts/header.php
@@ -7,7 +7,7 @@ use yii\helpers\Url;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '后台</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
