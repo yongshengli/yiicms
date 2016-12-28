3461e380fd518845dbac0f79f137f2ae723a71a9
liyongsheng <liyongsheng@meicai.cn>
2016-12-28 17:51:54 +0800

    edit backend 首页

--- modules/backend/views/default/index.php
+++ modules/backend/views/default/index.php
@@ -1,12 +1,39 @@
 <div class="backend-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">系统信息</h3>
+                </div>
+                <!-- /.box-header -->
+
+                <div class="box-body no-padding">
+                    <table class="table table-condensed">
+                        <tbody>
+                        <tr>
+                            <th style="width: 150px">Yii 版本</th>
+                            <td><?= Yii::getVersion() ?></td>
+                        </tr>
+                        <tr>
+                            <th>Yii ENV</th>
+                            <td><?= YII_ENV; ?></td>
+                        </tr>
+                        <tr>
+                            <th>Yii DUBUG</th>
+                            <td><?= YII_DEBUG; ?></td>
+                        </tr>
+                        <tr>
+                            <th>操作系统</th>
+                            <td><?= PHP_OS ?></td>
+                        </tr>
+                        <tr>
+                            <th>PHP 版本</th>
+                            <td><?= PHP_VERSION ?></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
