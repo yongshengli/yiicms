c1918877a6f6bb36ee061d32004480e0701c3129
liyongsheng <liyongsheng@meicai.cn>
2017-01-03 15:36:22 +0800

    edit style

--- modules/backend/views/layouts/sub-modules.php
+++ modules/backend/views/layouts/sub-modules.php
@@ -7,7 +7,11 @@
  * Email:liyongsheng@meicai.cn
  */
 ?>
+<style>
+    .box-body h1 {display:none}
+</style>
 <div class="box box-solid">
+    <div class="box-header with-border"><h3 class="box-title"><?=$this->title?></h3></div>
     <div class="box-body">
         <?= $content ?>
     </div>
