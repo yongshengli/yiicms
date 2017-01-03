baf68327fb6aa35a3f06aac5c3f525dacfe68b0a
liyongsheng <liyongsheng@meicai.cn>
2017-01-03 16:12:45 +0800

    fix bug

--- modules/backend/views/layouts/sub-modules.php
+++ modules/backend/views/layouts/sub-modules.php
@@ -6,10 +6,9 @@
  * Time: 14:55
  * Email:liyongsheng@meicai.cn
  */
+/** @var $this yii\web\View */
+$this->registerCss('.box-body h1 {display:none}');
 ?>
-<style>
-    .box-body h1 {display:none}
-</style>
 <div class="box box-solid">
     <div class="box-header with-border"><h3 class="box-title"><?=$this->title?></h3></div>
     <div class="box-body">
