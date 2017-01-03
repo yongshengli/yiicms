0bb2ebdc4552befc1a4c04ac915c295b39c06891
liyongsheng <liyongsheng@meicai.cn>
2017-01-03 15:17:35 +0800

    fix bug

---
+++ modules/backend/views/layouts/sub-modules.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 2017/1/3
+ * Time: 14:55
+ * Email:liyongsheng@meicai.cn
+ */
+?>
+<div class="box">
+    <div class="box-body">
+        <?= $content ?>
+    </div>
+</div>
