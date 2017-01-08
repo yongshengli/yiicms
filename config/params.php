a121b0e7dfde7b43f077dd9663de3f004d178d54
七觞酒 <739800600@qq.com>
2017-01-08 09:06:48 +0800

    fix bug

---
+++ config/params.php
@@ -0,0 +1,69 @@
+<?php 
+ return array (
+  'appName' => 'YiiCms企业管理系统',
+  'logo' => '@web/images/logo.png',
+  'keywords' => '',
+  'description' => '',
+  'cacheDuration' => '-1',
+  'pageSize' => '2',
+  'nav' => '{
+    "options": {
+        "class": "nav navbar-nav navbar-right"
+    },
+    "items": [
+        {
+            "label": "首页",
+            "url": [
+                "/site/index"
+            ]
+        },
+        {
+            "label": "产品",
+            "url": [
+                "/products/list"
+            ],
+            "activeUrls": [
+                "/products/index"
+            ]
+        },
+        {
+            "label": "新闻",
+            "url": [
+                "/news/list"
+            ],
+            "activeUrls": [
+                "/news/index"
+            ]
+        },
+        {
+            "label": "下载",
+            "url": [
+                "/downloads/list"
+            ],
+            "activeUrls": [
+                "/downloads/index"
+            ]
+        },
+        {
+            "label": "关于我们",
+            "url": [
+                "/site/about"
+            ],
+            "items": [
+                {
+                    "label": "企业荣誉",
+                    "url": {
+                        "0":"/site/page/honor", "id":"honor"
+                    }
+                }
+            ]
+        },
+        {
+            "label": "联系我们",
+            "url": [
+                "/site/contact"
+            ]
+        }
+    ]
+}',
+);
