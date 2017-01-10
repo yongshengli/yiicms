40e5cdcf96902f168b7b1a7a1d8a6a3dc7dfafbf
七觞酒 <739800600@qq.com>
2017-01-10 16:12:04 +0800

    fix bug

--- modules/backend/views/photos/view.php
+++ modules/backend/views/photos/view.php
@@ -1,11 +1,6 @@
 <?php
 
 use yii\helpers\Html;
-use kartik\form\ActiveForm;
-use kartik\file\FileInput;
-use kartik\file\CanvasBlobAsset;
-use kartik\file\SortableAsset;
-use kartik\file\DomPurifyAsset;
 use kartik\file\FileInputAsset;
 /* @var $this yii\web\View */
 /* @var $model app\models\Photos */
@@ -34,25 +29,21 @@ FileInputAsset::register($this);
             <div class="row">
                 <div class="col-lg-3"><img src="<?= $model->image ?>"/></div>
             </div>
-<!--            <div class="form-group">-->
-                <?php if (isset($detailModelList) && is_array($detailModelList)):?>
-            <div class="file-input" id="photo-list">
+            <div class="file-input">
                 <div class="file-preview">
                     <div class="file-drop-disabled">
                         <div class="file-preview-thumbnails">
                             <div class="file-initial-thumbs">
-                                <div class="clearfix">
-                                <?php foreach ($detailModelList as $item): ?>
+                                <div class="clearfix" id="photo-list">
+                                <?php if (isset($detailModelList) && is_array($detailModelList)):foreach ($detailModelList as $item): ?>
                                     <?= $this->render('_detail_item', ['model' => $item]) ?>
-                                <?php endforeach; ?>
+                                <?php endforeach; endif;?>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-<!--            </div>-->
             </div>
-            <?php endif;?>
         </div>
     </div>
 </div>
