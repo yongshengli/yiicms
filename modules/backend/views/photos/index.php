0b5eb5bd8802d7db2f122aea2002ef91dac613c0
七觞酒 <739800600@qq.com>
2017-01-11 15:22:51 +0800

    fix bug

--- modules/backend/views/photos/index.php
+++ modules/backend/views/photos/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'dataProvider' => $dataProvider,
                 'filterModel' => $searchModel,
                 'columns' => [
-                    ['class' => 'yii\grid\SerialColumn'],
+                    ['class' => 'yii\grid\CheckboxColumn'],
                     [
                         'attribute' => 'id',
                         'options' => ['style' => 'width:50px']
