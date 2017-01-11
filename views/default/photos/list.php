b4e685d370eb5886cfce0166603e9891b317da00
七觞酒 <739800600@qq.com>
2017-01-11 15:26:07 +0800

    fix bug

--- views/default/photos/list.php
+++ views/default/photos/list.php
@@ -36,7 +36,7 @@ $this->params['breadcrumbs'][] = $this->title;
             </div>
             <div class="col-lg-9">
                 <div class="panel panel-default panel-<?=\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')?>">
-                    <div class="panel-heading"><h3 class="panel-title">产品</h3></div>
+                    <div class="panel-heading"><h3 class="panel-title">相册</h3></div>
                 </div>
                 <div>
                     <?= ListView::widget([
