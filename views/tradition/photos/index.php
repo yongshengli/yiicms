b853f195759516995e8dad6c310f5d31ec0de7a3
七觞酒 <739800600@qq.com>
2017-01-11 15:46:29 +0800

    fix bug

---
+++ views/tradition/photos/index.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 2016/12/7
+ * Time: 10:55
+ * Email:liyongsheng@meicai.cn
+ */
+
+/* @var $this yii\web\View */
+/* @var $model \app\models\News */
+use yii\bootstrap\Html;
+use app\widgets\LastNews;
+use app\widgets\ConfigPanel;
+use yii\widgets\ListView;
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label'=>'企业相册', 'url'=>['/products/list']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<style>
+    .img-box{
+        height: 400px;width:400px; text-align: center;vertical-align: middle;display: table-cell;
+    }
+    .img-box img{
+        display: inline;
+        max-width:400px;max-height: 400px;
+    }
+</style>
+<div class="site-index">
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-3">
+                <?=\app\widgets\Category::widget(['type'=>\app\models\Content::TYPE_PRODUCTS,
+                    'options'=>['class'=>'panel panel-default panel-'.\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')]
+                ])?>
+                <?=\app\widgets\LastNews::widget(['options'=>['class'=>'panel panel-default panel-'.\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')]
+                ])?>
+                <?=\app\widgets\ConfigPanel::widget(['configName'=>'contact_us',
+                    'options'=>['class'=>'panel panel-default panel-'.\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')]
+                ])?>
+            </div>
+            <div class="col-lg-9">
+                <div class="panel-body text-center">
+                    <?= ListView::widget([
+                        'dataProvider' => $dataProvider,
+                        'layout' => "<div class='panel-body'>{items}</div>\n<div class='panel-body'>{pager}</div>",
+                        'itemView'=>function($item){
+                            $html ='<p><img src="'.$item->file_url.'"/></p>';
+                            $html .='<p class="text-left">'.$item->detail.'</p>';
+                            return $html;
+                        }
+                    ]); ?>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <?=$this->render('@app/views/news/_share')?>
+                        </div>
+                        <div class="col-lg-9 text-right">
+                            <?php if($previous = $model->previous()):?>
+                                上一相册 <?=Html::a($previous->title, ['/photos/index', 'id'=>$previous->id])?>
+                            <?php endif;?>
+                            <?php if($next = $model->next()):?>
+                                下一相册 <?=Html::a($next->title, ['/photos/index', 'id'=>$next->id])?>
+                            <?php endif;?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ views/tradition/photos/list.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 2016/12/7
+ * Time: 10:55
+ * Email:liyongsheng@meicai.cn
+ */
+
+/* @var $this yii\web\View */
+/** @var $dataProvider \yii\data\ActiveDataProvider */
+use yii\widgets\ListView;
+use yii\bootstrap\Html;
+
+$this->title = '企业相册';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<style>
+    .image-box a{
+        height: 240px;width:240px; text-align: center;vertical-align: middle;display: table-cell;
+    }
+    .image{max-width:100%;max-height:200px;vertical-align:middle;display: inline}
+</style>
+<div class="site-index">
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-3">
+                <?=\app\widgets\Category::widget(['type'=>\app\models\Content::TYPE_PRODUCTS,
+                    'options'=>['class'=>'panel panel-default panel-'.\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')]
+                ])?>
+                <?=\app\widgets\LastNews::widget(['options'=>['class'=>'panel panel-default panel-'.\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')]
+                ])?>
+                <?=\app\widgets\ConfigPanel::widget(['configName'=>'contact_us',
+                    'options'=>['class'=>'panel panel-default panel-'.\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')]
+                ])?>
+            </div>
+            <div class="col-lg-9">
+                <div class="panel panel-default panel-<?=\yii\helpers\ArrayHelper::getValue($this->params,'themeColor')?>">
+                    <div class="panel-heading"><h3 class="panel-title">相册</h3></div>
+                </div>
+                <div>
+                    <?= ListView::widget([
+                        'dataProvider' => $dataProvider,
+                        'layout' => "<div class='panel-body'>{items}</div>\n<div class='panel-body'>{pager}</div>",
+                        'itemView'=>'_item'
+                    ]); ?>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
