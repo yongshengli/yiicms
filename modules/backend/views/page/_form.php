e8a5201490b91bc2575f266b2ad63c56f059931c
七觞酒 <739800600@qq.com>
2017-01-25 12:32:36 +0800

    fix bug

--- modules/backend/views/page/_form.php
+++ modules/backend/views/page/_form.php
@@ -27,7 +27,7 @@ use yii\widgets\ActiveForm;
     ]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? '创建' : '修改', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
