93a48d0e15f70478b1853a1f899d907000600b7d
七觞酒 <739800600@qq.com>
2017-02-13 19:02:20 +0800

    fix bug

--- modules/backend/views/layouts/_menu_script.js
+++ modules/backend/views/layouts/_menu_script.js
@@ -3,17 +3,21 @@ $('#menu-keyword').autocomplete({
         var result = [];
         var limit = 10;
         var term = request.term.toLowerCase();
-        $.each(_menu_opts.menus, function () {
-            var menu = this;
+        $.each(_menu_opts.menus, function (index,menu) {
             if (term == '' || menu.label.toLowerCase().indexOf(term) >= 0 ||
                 (menu.label && menu.label.toLowerCase().indexOf(term) >= 0)) {
-                result.push(menu);
+                result.push({name:menu.label,"url":menu.url});
                 limit--;
                 if (limit <= 0) {
                     return false;
                 }
             }
         });
+        console.log(result);
         response(result);
     }
-});
+}).autocomplete("instance")._renderItem = function (ul, item) {
+    return $("<li>")
+        .append($('<a>').append($('<b>').text(item.name)))
+        .appendTo(ul);
+};
