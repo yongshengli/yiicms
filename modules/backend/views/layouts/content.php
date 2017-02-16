a071938ea90b0d02e7a59b5f9e1c13c7c7afdb9d
七觞酒 <739800600@qq.com>
2017-02-16 11:18:45 +0800

    fix bug

--- modules/backend/views/layouts/content.php
+++ modules/backend/views/layouts/content.php
@@ -38,11 +38,3 @@ use dmstr\widgets\Alert;
         <?php endif;?>
     </section>
 </div>
-
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2014-2015 <a href="http://yiicms.co">YiiCms</a>.</strong> All rights
-    reserved.
-</footer>
--- modules/backend/views/layouts/main.php
+++ modules/backend/views/layouts/main.php
@@ -46,6 +46,13 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed201
             'content.php',
             ['content' => $content, 'directoryAsset' => $directoryAsset]
         ) ?>
+        <footer class="main-footer">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 1.0
+            </div>
+            <strong>Copyright &copy; 2014-2015 <a href="http://yiicms.co">YiiCms</a>.</strong> All rights
+            reserved.
+        </footer>
     </div>
     <?php $this->endBody() ?>
     </body>
