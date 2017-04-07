5f27e9ad148b65330b202f368634be333705ad0c
七觞酒 <739800600@qq.com>
2017-04-07 18:21:51 +0800

    fix bug

--- config/web.php
+++ config/web.php
@@ -57,9 +57,9 @@ $config = [
                 'about'=>'site/about',
                 'contact'=>'site/contact',
                 'search'=>'site/search',
+                'page_<id:\d+>'=>'site/page',
                 '<controller:[\w-]+>/<id:\d+>'=>'<controller>/item',
                 '<controller:[\w-]+>/list_<category-id:\d+>'=>'<controller>/list',
-                'page_<id:\d+>'=>'site/page',
             ],
         ],
         'formatter'=>[
