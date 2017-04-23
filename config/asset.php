28c4c0918b3021857cce1c564ef1dd5265be4c98
七觞酒 <739800600@qq.com>
2017-04-23 10:19:43 +0800

    fix bug

---
+++ config/asset.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Configuration file for the "yii asset" console command.
+ */
+
+// In the console environment, some path aliases may not exist. Please define these:
+// Yii::setAlias('@webroot', __DIR__ . '/../web');
+// Yii::setAlias('@web', '/');
+
+return [
+    // Adjust command/callback for JavaScript files compressing:
+    'jsCompressor' => 'java -jar compiler.jar --js {from} --js_output_file {to}',
+    // Adjust command/callback for CSS files compressing:
+    'cssCompressor' => 'java -jar yuicompressor.jar --type css {from} -o {to}',
+    // Whether to delete asset source after compression:
+    'deleteSource' => false,
+    // The list of asset bundles to compress:
+    'bundles' => [
+        // 'app\assets\AppAsset',
+        // 'yii\web\YiiAsset',
+        // 'yii\web\JqueryAsset',
+    ],
+    // Asset bundle for compression output:
+    'targets' => [
+        'all' => [
+            'class' => 'yii\web\AssetBundle',
+            'basePath' => '@webroot/assets',
+            'baseUrl' => '@web/assets',
+            'js' => 'js/all-{hash}.js',
+            'css' => 'css/all-{hash}.css',
+        ],
+    ],
+    // Asset manager configuration:
+    'assetManager' => [
+        //'basePath' => '@webroot/assets',
+        //'baseUrl' => '@web/assets',
+    ],
+];
