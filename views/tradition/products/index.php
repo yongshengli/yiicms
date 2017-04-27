30ed1c6a506e7a8f5f2213b62c1696f643abb1e1
七觞酒 <739800600@qq.com>
2017-04-27 17:32:59 +0800

    fix bug

--- views/tradition/products/index.php
+++ views/tradition/products/index.php
@@ -19,12 +19,11 @@ $this->params['breadcrumbs']=[];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <style>
-
     @media (min-width:768px) {
-        .img-rounded{margin-left: auto;margin-right:auto;width:420px;padding: 15px;max-width: 100%}
+        .img-rounded{margin-left: auto;margin-right:auto;width:380px;padding: 15px;max-width: 100%}
         .img-box{
             background-color: #ebebeb;
-            height: 400px;width:400px; text-align: center;vertical-align: middle;display: table-cell;
+            height: 360px;width:360px; text-align: center;vertical-align: middle;display: table-cell;
         }
         .img-box img{
             display: inline;
