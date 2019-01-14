d9da30104866f1021d29739c059478ae7d916b68
七觞酒 <739800600@qq.com>
2019-01-14 18:38:38 +0800

    fix bug

--- web/index.php
+++ web/index.php
@@ -1,8 +1,9 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+define('YII_DEBUG', true);
+define('YII_ENV', 'dev');
+define('ASSETS_DIR', __DIR__.'/assets');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
