5b1365685be817cce19178e512a3aa27883b1718
七觞酒 <739800600@qq.com>
2020-02-03 18:01:51 +0800

    fix bug

--- web/index.php
+++ web/index.php
@@ -3,13 +3,13 @@
 // comment out the following two lines when deployed to production
 define('YII_DEBUG', true);
 define('YII_ENV', 'dev');
+define('BASE_PATH', realpath(dirname(__DIR__)));
+
 //define('ASSETS_DIR', __DIR__.'/assets');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-
-$config = require(__DIR__ . '/../config/web.php');
-
+require(BASE_PATH .'/vendor/autoload.php');
+require(BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
+$config = require(BASE_PATH . '/config/web.php');
 $app = new yii\web\Application($config);
 $app->language = Yii::$app->session->get('language', 'zh-CN');
 $app->run();
