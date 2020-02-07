912293a4666287356b12822f4d463c7667fc6f76
七觞酒 <739800600@qq.com>
2020-02-07 17:59:33 +0800

    fix bug

--- web/index.php
+++ web/index.php
@@ -11,5 +11,5 @@ require(BASE_PATH .'/vendor/autoload.php');
 require(BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
 $config = require(BASE_PATH . '/config/web.php');
 $app = new yii\web\Application($config);
-//$app->language = Yii::$app->session->get('language', 'zh-CN');
+$app->language = Yii::$app->session->get('language', 'zh-CN');
 $app->run();
